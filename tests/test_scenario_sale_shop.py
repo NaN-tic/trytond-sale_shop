52568d8adc1faa1bbe79c44fe358deca16cbbec9
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-09 10:36:14 +0200

    Convert .rst to .py

---
+++ tests/test_scenario_sale_shop.py
@@ -0,0 +1,146 @@
+import unittest
+from decimal import Decimal
+
+from proteus import Model
+from trytond.modules.account.tests.tools import (create_chart,
+                                                 create_fiscalyear, create_tax,
+                                                 get_accounts)
+from trytond.modules.account_invoice.tests.tools import (
+    create_payment_term, set_fiscalyear_invoice_sequences)
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.modules.sale_shop.tests.tools import create_shop
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules, set_user
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
+    def test(self):
+
+        # Install sale
+        config = activate_modules('sale_shop')
+
+        # Create company
+        _ = create_company()
+        company = get_company()
+
+        # Create fiscal year
+        fiscalyear = set_fiscalyear_invoice_sequences(
+            create_fiscalyear(company))
+        fiscalyear.click('create_period')
+
+        # Create chart of accounts
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
+
+        # Create tax
+        tax = create_tax(Decimal('.10'))
+        tax.save()
+
+        # Create payment term
+        payment_term = create_payment_term()
+        payment_term.save()
+
+        # Create parties
+        Party = Model.get('party.party')
+        supplier = Party(name='Supplier')
+        supplier.save()
+        customer = Party(name='Customer')
+        customer.save()
+
+        # Create category
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name='Category')
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.customer_taxes.append(tax)
+        account_category.save()
+
+        # Create product
+        ProductUom = Model.get('product.uom')
+        unit, = ProductUom.find([('name', '=', 'Unit')])
+        ProductTemplate = Model.get('product.template')
+        template = ProductTemplate()
+        template.name = 'product'
+        template.default_uom = unit
+        template.type = 'goods'
+        template.list_price = Decimal('40')
+        template.account_category = account_category
+        template.salable = True
+        template.save()
+        product, = template.products
+        product.cost_price = Decimal('25')
+        product.save()
+
+        # Create product price list
+        PriceList = Model.get('product.price_list')
+        price_list = PriceList(name='Retail', price='list_price')
+        price_list_line = price_list.lines.new()
+        price_list_line.quantity = 10.0
+        price_list_line.product = product
+        price_list_line.formula = 'unit_price * 0.7'
+        price_list_line = price_list.lines.new()
+        price_list_line.product = product
+        price_list_line.formula = 'unit_price * 0.8'
+        price_list_line = price_list.lines.new()
+        price_list_line.formula = 'unit_price * 0.5'
+        price_list.save()
+
+        # Create an Inventory
+        Inventory = Model.get('stock.inventory')
+        InventoryLine = Model.get('stock.inventory.line')
+        Location = Model.get('stock.location')
+        storage, = Location.find([
+            ('code', '=', 'STO'),
+        ])
+        inventory = Inventory()
+        inventory.location = storage
+        inventory.save()
+        inventory_line = InventoryLine(product=product, inventory=inventory)
+        inventory_line.quantity = 100.0
+        inventory_line.expected_quantity = 0.0
+        inventory.save()
+        inventory_line.save()
+        Inventory.confirm([inventory.id], config.context)
+        self.assertEqual(inventory.state, 'done')
+
+        # Create Sale Shop
+        shop = create_shop(payment_term, price_list)
+        shop.save()
+
+        # Save Sale Shop User
+        User = Model.get('res.user')
+        user, = User.find([])
+        user.shops.append(shop)
+        user.shop = shop
+        user.save()
+        set_user(user)
+
+        # Sale 5 products
+        Sale = Model.get('sale.sale')
+        SaleLine = Model.get('sale.line')
+        sale = Sale()
+        sale.party = customer
+        sale.payment_term = payment_term
+        sale.invoice_method = 'shipment'
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.product = product
+        sale_line.quantity = 2.0
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.product = product
+        sale_line.quantity = 3.0
+        sale.save()
+        self.assertEqual(sale.state, 'draft')
